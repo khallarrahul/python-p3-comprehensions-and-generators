51a3fe6808f29d9f69e99c4491483ffff169907e
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-07 16:17:39 -0700

    resolves

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    even_lc = [n for n in num_list if n % 2 == 0]
+    print(even_lc)
+    return even_lc
+
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
+
 
 def make_exclamation(sentence_list):
-    pass
+    word_lc = [word + "!" for word in sentence_list]
+    print(word_lc)
+    return word_lc
+
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
